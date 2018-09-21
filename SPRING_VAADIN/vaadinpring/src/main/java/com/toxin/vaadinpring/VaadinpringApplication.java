2bb1a391cd958c6c82097a3c9f1650a84853446d
toxin <yuri.belousov.1998@gmail.com>
2018-09-21 21:48:10 +0300

    init vaadin

---
+++ SPRING_VAADIN/vaadinpring/src/main/java/com/toxin/vaadinpring/VaadinpringApplication.java
@@ -0,0 +1,12 @@
+package com.toxin.vaadinpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinpringApplication.class, args);
+    }
+}
