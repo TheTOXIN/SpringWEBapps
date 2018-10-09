a337b4772ead2e9455cbe49ad437a3749f89a941
toxin <yuri.belousov.1998@gmail.com>
2018-10-09 18:45:12 +0300

    add ribbon

--- SPRING_CLOUD/service-conversion/src/main/java/com/toxin/serviceconversion/CurrencyExchangeServiceProxy.java
+++ SPRING_CLOUD/service-conversion/src/main/java/com/toxin/serviceconversion/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,12 @@
 package com.toxin.serviceconversion;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="service-forex", url="localhost:8000")
+@FeignClient(name="service-forex")
+@RibbonClient(name="service-forex")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
