dc7a89863f6af6cccb99f6840c3a9e89dec1a119
toxin <yuri.belousov.1998@gmail.com>
2018-10-09 18:01:00 +0300

    add forex service

---
+++ SPRING_CLOUD/service-forex/src/main/java/com/toxin/serviceforex/ServiceForexApplication.java
@@ -0,0 +1,13 @@
+package com.toxin.serviceforex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceForexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceForexApplication.class, args);
+	}
+
+}
